fa4ff82e9b413fc59927bdd30cbd0676f7f92d3b
midhunkollillath <108817703+midhunkollillath@users.noreply.github.com>
2023-08-06 12:16:48 +0530

    Update server.js
    yes

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ dotenv.config();
 //databse config
 connectDB();
 //es module file
-const __filename = fileURLToPath{import.meta.url}
+const __filename = fileURLToPath(import.meta.url);
+
 
 const __dirname = path.dirname(__filename);
 //rest object
